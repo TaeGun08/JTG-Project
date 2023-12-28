5642966cd6aed1895a05df80668de6cbcbc92178
Hyuns008 <gustn4984@naver.com>
2023-12-28 22:51:12 +0900

    스킬 쿨타임 이미지화

--- Assets/Scripts/Skill/PlayerSkill/PlayerSkill.cs
+++ Assets/Scripts/Skill/PlayerSkill/PlayerSkill.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerSkill : MonoBehaviour
 {
@@ -12,12 +15,26 @@ public class PlayerSkill : MonoBehaviour
 
     private Player player;
 
+    [Header("스킬 UI관련 데이터")]
+    [SerializeField] private GameObject playerSkill;
+    [SerializeField] private GameObject skillCoolTimePanel;
+    [SerializeField] private Image coolTimePanelImage;
+    [SerializeField] private GameObject skillCollTimeText;
+    [SerializeField] private TMP_Text coolTimeText;
+    private float coolTime;
+
     [Header("스킬 프리팹")]
     [SerializeField] private List<GameObject> skillPrefabs = new List<GameObject>();
 
     [Header("스킬이 나타날 위치 및 회전 값")]
     [SerializeField] private Transform skillPos;
     [SerializeField] private Transform skillRot;
+
+    [Header("스킬 설정")]
+    [SerializeField] private float skillCoolTime = 1.0f;
+    private float skillCoolTimer = 0.0f;
+    private bool skillCoolOn = false;
+
 
     private void Start()
     {
@@ -27,11 +44,42 @@ public class PlayerSkill : MonoBehaviour
         trashPreFab = TrashPreFab.instance;
 
         player = GetComponent<Player>();
+
+        skillCoolTimer = skillCoolTime;
+
+        playerSkill.SetActive(true);
+        skillCollTimeText.SetActive(false);
+        skillCoolTimePanel.SetActive(false);
+
+        coolTime = coolTimePanelImage.fillAmount;
     }
 
     private void Update()
     {
+        skillCool();
         playerSpecialAttack();
+    }
+
+    /// <summary>
+    /// 스킬 사용 시 쿨타임들
+    /// </summary>
+    private void skillCool()
+    {
+        if (skillCoolOn == true)
+        {
+            string timerText = $"{(int)skillCoolTimer + 1}";
+            coolTimeText.text = timerText;
+            skillCoolTimer -= Time.deltaTime;
+            coolTimePanelImage.fillAmount = skillCoolTimer;
+            if (skillCoolTimer < 0.0f)
+            {
+                skillCoolOn = false;
+                skillCollTimeText.SetActive(false);
+                skillCoolTimePanel.SetActive(false);
+                coolTimePanelImage.fillAmount = coolTime;
+                skillCoolTimer = skillCoolTime;
+            }
+        }
     }
 
     /// <summary>
@@ -39,16 +87,20 @@ public class PlayerSkill : MonoBehaviour
     /// </summary>
     private void playerSpecialAttack()
     {
-        if (Input.GetKeyDown(keyManager.PlayerSpecialAttackKey()))
+        if (Input.GetKeyDown(keyManager.PlayerSpecialAttackKey()) && skillCoolOn == false)
         {
+            skillCoolOn = true;
+            skillCollTimeText.SetActive(true);
+            skillCoolTimePanel.SetActive(true);
+
             Player.PlayerSkillType skillType = player.SkillType();
             if (skillType.ToString() == "skillTypeA")
             {
                 GameObject skillObj = Instantiate(skillPrefabs[0], skillPos.position,
-                    Quaternion.identity, trashPreFab.transform);
+                    skillRot.rotation, trashPreFab.transform);
                 Knife knifeSc = skillObj.GetComponent<Knife>();
 
-                knifeSc.KnifeForce(new Vector2(15.0f, 0f), player.playerMouseAimRight());
+                knifeSc.KnifeForce(skillRot.rotation * (new Vector2(15.0f, 0f)), player.playerMouseAimRight());
             }
             else if (skillType.ToString() == "skillTypeB")
             {
