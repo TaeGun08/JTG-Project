b3de9a1fe8f835a0d85f7f3039144c0538d32f0a
Hyuns008 <gustn4984@naver.com>
2024-01-17 00:12:08 +0900

    보스 패턴 2개 추가

--- Assets/JTGProjectAssets/Scripts/Boss/Boss.cs
+++ Assets/JTGProjectAssets/Scripts/Boss/Boss.cs
@@ -25,14 +25,26 @@ public class Boss : MonoBehaviour
     private float gravityVelocity;
 
     [Header("보스 공격 설정")]
-    [SerializeField, Tooltip("보스의 기본 공격을 위한 영역")] private List<BoxCollider2D> pattern1Attack;
+    [SerializeField, Tooltip("보스의 기본 공격을 위한 영역")] private BoxCollider2D pattern1AttackCheck;
     [SerializeField, Tooltip("보스의 패턴2 공격을 위한 영역")] private BoxCollider2D pattern2Attack;
     [SerializeField, Tooltip("보스의 패턴3 공격을 위한 영역")] private BoxCollider2D pattern3Attack;
+    [SerializeField, Tooltip("보스의 기본 공격 범위")] private BoxCollider2D pattern1Attack;
     [SerializeField, Tooltip("보스의 패턴3의 공격 프리팹")] private GameObject patternAttackPrefab;
     private bool isAttack = false;
     private bool patternAttack = false;
-    private int randomPattern;
-    private float attackDelay;
+    [SerializeField] private int randomPattern;
+    private float attack1DelayTimer;
+    private float attack2DelayTimer;
+    private float attack3DelayTimer;
+    private bool attack1DelayOn = false;
+    private bool attack2DelayOn = false;
+    private bool attack3DelayOn = false;
+    [Space]
+    [SerializeField, Tooltip("보스가 두 번째 패턴을 사용 후 재사용 대기 시간")] private float pattern2CoolTime;
+    [SerializeField, Tooltip("보스가 세 번째 패턴을 사용 후 재사용 대기 시간")] private float pattern3CoolTime;
+    private bool usePattern2 = false;
+    private bool usePattern3 = false;
+    private Transform playerTrs;
 
     private void OnDrawGizmos() //박스캐스트를 씬뷰에서 눈으로 확인이 가능하게 보여줌
     {
@@ -48,7 +60,12 @@ public class Boss : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            pattern1Attack[1].gameObject.SetActive(true);
+            if (isAttack == false && randomPattern == 0)
+            {
+                anim.SetTrigger("isPatternA");
+                attack1DelayOn = true;
+                isAttack = true;
+            }
         }
     }
 
@@ -56,7 +73,18 @@ public class Boss : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            pattern1Attack[1].gameObject.SetActive(true);
+            if (usePattern2 == true && randomPattern == 1)
+            {
+                return;
+            }
+
+            if (isAttack == false && curPhase >= 1 && randomPattern == 1)
+            {
+                anim.SetBool("isPatternB_bool", true);
+                playerTrs = collision.transform;
+                attack2DelayOn = true;
+                usePattern2 = true;
+            }
         }
     }
 
@@ -64,19 +92,33 @@ public class Boss : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            isAttack = true;
+            if (usePattern3 == true && randomPattern == 2)
+            {
+                return;
+            }
+
+            if (isAttack == false && curPhase == 2 && randomPattern == 2)
+            {
+                anim.SetTrigger("isPatternC");
+                attack3DelayOn = true;
+                playerTrs = collision.transform;
+                usePattern3 = true;
+                isAttack = true;
+            }
         }
     }
 
-    private void onTriggerAttackCheck(Collider2D collision)
+    private void onTriggerAttackArea(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {          
-            anim.SetTrigger("isPatternA");
-            //anim.SetBool("isAttack", true);
-            //pattern1Attack[1].gameObject.SetActive(false);
-            //isAttack = true;
-        }
+        {
+            if (patternAttack == true)
+            {
+                Player playerSc = collision.gameObject.GetComponent<Player>();
+                playerSc.PlayerCurHp(10, true, false);
+                patternAttack = false;
+            }
+        }        
     }
 
     private void Awake()
@@ -87,8 +129,6 @@ public class Boss : MonoBehaviour
         bossCurHp = bossPhaseHp[0];
 
         changeTimer = 0;
-
-        pattern1Attack[1].gameObject.SetActive(false);
     }
 
     private void Start()
@@ -117,8 +157,8 @@ public class Boss : MonoBehaviour
 
     private void playerCollCheck()
     {
-        Collider2D pattern1 = Physics2D.OverlapBox(pattern1Attack[0].bounds.center,
-            pattern1Attack[0].bounds.size, 0, LayerMask.GetMask("Player"));
+        Collider2D pattern1 = Physics2D.OverlapBox(pattern1AttackCheck.bounds.center,
+            pattern1AttackCheck.bounds.size, 0, LayerMask.GetMask("Player"));
 
         Collider2D pattern2 = Physics2D.OverlapBox(pattern2Attack.bounds.center,
             pattern2Attack.bounds.size, 0, LayerMask.GetMask("Player"));
@@ -126,57 +166,27 @@ public class Boss : MonoBehaviour
         Collider2D pattern3 = Physics2D.OverlapBox(pattern3Attack.bounds.center,
             pattern3Attack.bounds.size, 0, LayerMask.GetMask("Player"));
 
-        Collider2D pattern1Att = Physics2D.OverlapBox(pattern1Attack[1].bounds.center,
-            pattern1Attack[1].bounds.size, LayerMask.GetMask("Player"));
+        Collider2D patternAtt = Physics2D.OverlapBox(pattern1Attack.bounds.center,
+            pattern1Attack.bounds.size, 0, LayerMask.GetMask("Player"));
 
-
-        if (patternAttack == false && curPhase == 0)
+        if (pattern1 != null)
         {
-            if (pattern1 != null && isAttack == false)
-            {
-                onTriggerArea1Check(pattern1);
-            }
-
-            if (pattern1Att != null && isAttack == false)
-            {
-                onTriggerAttackCheck(pattern1Att);
-            }
+            onTriggerArea1Check(pattern1);
         }
-        else if (patternAttack == true && curPhase == 1)
-        {
-            if (pattern1 != null && isAttack == false)
-            {
-                onTriggerArea1Check(pattern1);
-            }
-            else if (pattern2 != null && isAttack == false)
-            {
-                onTriggerArea2Check(pattern2);
-            }
 
-            if (pattern1Att != null && isAttack == false)
-            {
-                onTriggerAttackCheck(pattern1Att);
-            }
+        if (pattern2 != null)
+        {
+            onTriggerArea2Check(pattern2);
         }
-        else if (patternAttack == true && curPhase == 2)
-        {
-            if (pattern1 != null && isAttack == false)
-            {
-                onTriggerArea1Check(pattern1);
-            }
-            else if (pattern2 != null && isAttack == false)
-            {
-                onTriggerArea2Check(pattern2);
-            }
-            else if (pattern3 != null && isAttack == false)
-            {
-                onTriggerArea3Check(pattern3);
-            }
 
-            if (pattern1Att != null && isAttack == false)
-            {
-                onTriggerAttackCheck(pattern1Att);
-            }
+        if (pattern3 != null)
+        {
+            onTriggerArea3Check(pattern3);
+        }
+
+        if (patternAtt != null)
+        {
+            onTriggerAttackArea(patternAtt);
         }
     }
 
@@ -204,11 +214,48 @@ public class Boss : MonoBehaviour
 
         if (isAttack == true)
         {
-            attackDelay += Time.deltaTime;
-            if (attackDelay > 3)
+            attack1DelayTimer += Time.deltaTime;
+            if (attack1DelayTimer > 3)
             {
-                anim.SetBool("isAttack", false);
+                attack1DelayTimer = 0;
+                if (curPhase == 1)
+                {
+                    int randomPat = Random.Range(0, 2);
+                    randomPattern = randomPat;
+                }
+                else if (curPhase == 2)
+                {
+                    int randomPat = Random.Range(0, 3);
+                    randomPattern = randomPat;
+                }
                 isAttack = false;
+            }
+        }
+
+        if (attack1DelayOn == true)
+        {
+            attack1DelayTimer += Time.deltaTime;
+            if (attack1DelayTimer > 0.8f)
+            {
+                attack1DelayTimer = 0;
+                attack1DelayOn = false;
+                patternAttack = true;
+            }
+        }
+
+        if (attack2DelayOn == true)
+        {
+
+        }
+
+        if (attack3DelayOn == true)
+        {
+            attack3DelayTimer += Time.deltaTime;
+            if (attack3DelayTimer > 0.8f)
+            {
+                Instantiate(patternAttackPrefab, playerTrs.position + new Vector3(0, 2.5f, 0), Quaternion.identity, transform);
+                attack3DelayTimer = 0;
+                attack3DelayOn = false;
             }
         }
     }
